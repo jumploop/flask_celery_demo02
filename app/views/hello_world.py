83e79ea4085c7521db75b1c735bfa61bcc123cca
Sourcery AI <>
2022-04-11 15:12:15 +0000

    'Refactored by Sourcery'

--- app/views/hello_world.py
+++ app/views/hello_world.py
@@ -5,7 +5,7 @@ from app.tasks import add_together, mul_together
 @view.route('/', methods=['GET', 'POST'])
 def index():
     result = add_together.delay(23, 42)
-    print("task_id:{}".format(result))
+    print(f"task_id:{result}")
     result2 = mul_together.delay(2, 10)
     print(result2)
     return "add_together and mul_together is running...."
